6742cda3e0eea35f3f36dbbfc454370b4888fd52
Siddhesh <64964929+sid-py@users.noreply.github.com>
2021-07-01 09:25:41 +0200

    Updated the score condition on line 31

--- day_3/love_calculator.py
+++ day_3/love_calculator.py
@@ -28,7 +28,7 @@ score = int(str(score_TRUE) + str(score_LOVE)) # String concatenation and int co
 
 if score < 10 or score > 90:
     print(f"Your score is {score}, you go together like coke and mentos.")
-elif score > 40 and score < 50:
+elif score >= 40 and score <= 50:
     print(f"Your score is {score}, you are alright together.")
 else:
     print(f"Your score is {score}.")
