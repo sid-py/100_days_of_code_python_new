63a54dd4d738dbcd48b2d96b17e7286501bab172
Siddhesh <64964929+sid-py@users.noreply.github.com>
2021-07-05 18:06:11 +0200

    Create func_review.py

---
+++ day_8/func_review.py
@@ -0,0 +1,14 @@
+# Review: 
+# Create a function called greet(). 
+
+def greet(name, location):
+    # Write 3 print statements inside the function.
+    print(f"Hello {name}")
+    print(f"Good Morning! {name}")
+    print(f"How ya doin, {name}?")
+    print(f"Do you live in {location}?")
+# Call the greet() function and run your code.
+
+greet("Siddhesh", "Radolfzell")
+
+greet("Jane", "USA")
