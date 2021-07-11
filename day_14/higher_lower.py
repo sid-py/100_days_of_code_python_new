cefe07ebcd2c5bd587e1e0224853a0a2158333a3
Siddhesh <64964929+sid-py@users.noreply.github.com>
2021-07-11 23:16:18 +0200

    New

---
+++ day_14/higher_lower.py
@@ -0,0 +1,19 @@
+# Import all the required modules
+from game_data import data
+
+# print the Logo and welcoming message
+
+# Print compare A sentence
+#  Parse the list randomly for the dictionary entries and construct the sentence
+
+# print VS logo
+
+#  Print compare B sentence
+
+#  Ask for user inout to select A or B
+
+# check if correct
+
+# If guess is correct, A == B and game continues
+
+# If guess is incorrect, print losing message and value if correct guesses
