56c6aa87880d28c2105c0e15f09268229467499c
Siddhesh <64964929+sid-py@users.noreply.github.com>
2021-07-12 00:22:25 +0200

    Higher lower game

--- day_14/higher_lower.py
+++ day_14/higher_lower.py
@@ -1,22 +1,37 @@
-# Import all the required modules
+# Importing all the required modules
 from game_data import data
 import random
 from art import logo, vs
 
 # print the Logo and welcoming message
 print(logo)
+position_A = random.randint(1,len(data)) # Defining random position A for the list index
+should_continue = True # A temporary variable for While loop
 
-# Print compare A sentence
-#  Parse the list randomly for the dictionary entries and construct the sentence
+score = 0 # Variable to track the score
 
-# print VS logo
-
-#  Print compare B sentence
-
-#  Ask for user inout to select A or B
-
-# check if correct
-
-# If guess is correct, A == B and game continues
-
-# If guess is incorrect, print losing message and value if correct guesses
+while should_continue: # While loop
+    position_B = random.randint(1,len(data)) # Defining random position B for the list index
+    print(f"""Compare A: {data[position_A]["name"]}, a {data[position_A]["description"]}, from {data[position_A]["country"]}""") # Printing the statements
+    print(vs) # Printing the VS logo
+    print(f"""Compare B: {data[position_B]["name"]}, a {data[position_B]["description"]}, from {data[position_B]["country"]}""")# Printing the statements
+    guess = input("Who has more followers? Type 'A' or 'B': ").upper() # Prompt for the user input
+    # Checking the condition
+    if guess == "A": # Checking if A is greater than B. If yes, messages are printied and positions are updated
+        if data[position_A]["follower_count"] > data[position_B]["follower_count"]: 
+            score += 1
+            print(f"You are right! Current score: {score}")
+            position_A = position_A
+        else:
+            print(f"Sorry, that's wrong! Your score is {score}.")
+            should_continue = False
+    elif guess == "B": # Checking if B is greater than A. If yes, messages are printied and positions are updated   
+        if data[position_A]["follower_count"] < data[position_B]["follower_count"]:
+            score += 1
+            print(f"You are right! Current score: {score}")
+            position_A = position_B
+        else:
+            print(f"Sorry, that's wrong! Your score is {score}.")
+            should_continue = False
+            
+# Program successfully built in the first attempt!
