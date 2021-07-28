dbd21c8248fe7c26c0a7638258b1285ef1b8fb27
Siddhesh <64964929+sid-py@users.noreply.github.com>
2021-07-28 12:19:11 +0200

    wip

--- day_20/scoreboard.py
+++ day_20/scoreboard.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 ALIGNMENT ="center"
 FONT = ("Arial", 24, "normal")
 
-with open(r"C:\Users\2kjph5\OneDrive - Merit Automotive Electronics Systems, S.L\03-Miscellaneous\Studies\Learning\python\100_days_of_code_python_new\day_20\data.txt",mode="r") as file:
+with open(r"C:\Users\2kjph5\OneDrive - Merit Automotive Electronics Systems, S.L\03-Miscellaneous\Studies\Learning\python\100_days_of_code_python_new\day_20\data.txt",mode= "r+") as file:
     content = (file.read())
    
     
@@ -24,7 +24,7 @@ class Scoreboard(Turtle):
     
     def reset(self):
         if self.score > self.highscore:
-            file.write(self.highscore)
+            file.write(self.score)
         self.clear()    
         self.score = 0
         self.update_scoreboard()
