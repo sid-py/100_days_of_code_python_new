09d2882ff3d8d5b4c8f629ce663bfab165c71854
Siddhesh <64964929+sid-py@users.noreply.github.com>
2021-07-28 11:56:11 +0200

    writing

--- day_24/main.py
+++ day_24/main.py
@@ -1,4 +1,5 @@
-with open(r"""C:\Users\2kjph5\OneDrive - Merit Automotive Electronics Systems, S.L\03-Miscellaneous\Studies\Learning\python\100_days_of_code_python_new\day_24\my_file.txt""") as file:
+with open(r"""C:\Users\2kjph5\OneDrive - Merit Automotive Electronics Systems, S.L\03-Miscellaneous\Studies\Learning\python\100_days_of_code_python_new\day_24\my_file.txt""", mode= "a") as file:
+    file.write("I am an Engineer")
     contents = file.read()
 
     print(contents)
