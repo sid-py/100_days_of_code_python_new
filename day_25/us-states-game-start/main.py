b464ac73aa17bf2409f3c69bc517b714b0190907
Siddhesh <64964929+sid-py@users.noreply.github.com>
2021-08-01 18:00:40 +0200

    States name guessing game completed

--- day_25/us-states-game-start/main.py
+++ day_25/us-states-game-start/main.py
@@ -1,3 +1,11 @@
+"""
+This game shows a map of USA and prompts the user to guess the name of states. If the name is correct, it is shown 
+on the map. If entered "Exit", the game ends and the list of remaining states are saved in a separate .csv file
+as an aid for the user to learn and improve the name of states.
+
+The game if programmed through Python's Turtle module.
+"""
+
 import turtle
 import pandas as pd
 
@@ -16,12 +24,19 @@ turtle.shape(image)
 data = pd.read_csv(r"day_25\us-states-game-start\50_states.csv")
 all_states = data["state"].to_list()
 guessed_state =[]
+missing_states = []
 
 while len(guessed_state) < 50:
     answer_state = screen.textinput(title = f"{len(guessed_state)}/50", prompt = "What's another state's name?").title()
     print(answer_state)
 
     if answer_state == "Exit":
+        for state in all_states:
+            if state not in guessed_state:
+                missing_states.append(state)
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv(r"day_25\us-states-game-start\states_to_learn.csv")        
+        print(missing_states)
         break
     if answer_state in all_states:
         guessed_state.append(answer_state)
@@ -33,9 +48,8 @@ while len(guessed_state) < 50:
         t.write(answer_state)
 
 
-states_to_learn = all_states - guessed_state
-print(states_to_learn)
-# states_to_learn.csv
+
+
 
 
 
