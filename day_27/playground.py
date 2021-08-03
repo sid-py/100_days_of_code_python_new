417d95742e60995e76b362229a15e2a4aaf44093
Siddhesh <64964929+sid-py@users.noreply.github.com>
2021-08-03 18:55:34 +0200

    Update playground.py

--- day_27/playground.py
+++ day_27/playground.py
@@ -1,8 +1,16 @@
-def add(*args):
-    sum = 0
-    for i in args:
-        sum += i
-    return sum
+# def add(*args):
+#     sum = 0
+#     for i in args:
+#         sum += i
+#     return sum
     
     
-print(add(1,2,3.78,89,33,65))
+# print(add(1,2,3.78,89,33,65))
+
+
+def calculate(n, **kw):
+    n += kw.get("add")
+    n *= kw.get("multiply")
+    print(n)
+    
+calculate(3, add = 5)    
