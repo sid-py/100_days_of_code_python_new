3e02208d9c398f59b2e2d680a54683bd9fe66d52
Siddhesh <64964929+sid-py@users.noreply.github.com>
2021-08-12 00:15:03 +0200

    Pomodoro App completed

--- day_28/main.py
+++ day_28/main.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+import math
+from typing import Text
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,6 +11,8 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 TICK = "✓"
 
@@ -15,18 +20,71 @@ BUTTON_FONT_SIZE= (FONT_NAME, 8)
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
+def reset_timer():
+    window.after_cancel(timer)
+    # timer_text 00:00
+    canvas.itemconfig(timer_text, text = "00:00")
+    # title_label "Timer"
+    label.config(text = "Timer", fg = GREEN)
+    
+    global reps
+    reps = 0
+    
+    # reset check marks
+    tick.config(text = "")
+    
+
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
-    count_down(5)
+    global reps
+    reps += 1
+    
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    
+    if reps % 8 ==0:
+        count_down(long_break_sec)
+        label.config(text="Break", fg = RED)
+        
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        label.config(text="Break", fg = PINK)
+        
+    else:
+        count_down(work_sec)
+        label.config(text="Work", fg = GREEN)
     
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    canvas.itemconfig(timer_text, text = count)
+    
+    # 00:00
+    
+    count_min = math.floor(count / 60)
+    count_sec = count % 60 
+    
+    if count_sec <= 9:
+        count_sec = f"0{count_sec}"
+        
+    if count_min <= 9:
+        count_min = f"0{count_min}"
+    
+        
+    canvas.itemconfig(timer_text, text = f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1 )
+        global timer
+        timer = window.after(1000, count_down, count - 1 )
+        
+    else:
+        start_timer()
+        marks = ""
+        for _ in range(math.floor(reps/2)):
+            marks += TICK
+        tick.config(text = marks)
+            
     
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -53,13 +111,11 @@ tick.grid(column=3, row=6)
 
 # Defining the functions for the buttons
 
-button = tk.Button(text="Start", command = start_timer, font = BUTTON_FONT_SIZE, highlightthickness=0)
-button.grid(column=2, row=5)
+start_button = tk.Button(text="Start", command = start_timer, font = BUTTON_FONT_SIZE, highlightthickness=0)
+start_button.grid(column=2, row=5)
 
-def reset():
-    pass
-button = tk.Button(text="Reset", command = reset, font = BUTTON_FONT_SIZE, highlightthickness= 0)
-button.grid(column=4, row=5)
+reset_button = tk.Button(text="Reset", command = reset_timer, font = BUTTON_FONT_SIZE, highlightthickness= 0)
+reset_button.grid(column=4, row=5)
 
 
 
