055b052fc68ee0ad728ff54c36d650c05602c498
Siddhesh <64964929+sid-py@users.noreply.github.com>
2021-08-24 23:08:59 +0200

    Quizzler app completed

--- day_34/quizzler/main.py
+++ day_34/quizzler/main.py
@@ -24,7 +24,7 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
-quiz_ui = QuizInterface(quiz_brain=QuizBrain)
+quiz_ui = QuizInterface(quiz)
 
 # while quiz.still_has_questions():
 #     quiz.next_question()
