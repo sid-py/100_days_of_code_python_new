e69322e1ae8eb4578b265f82c838447ad6c7231d
Siddhesh <64964929+sid-py@users.noreply.github.com>
2021-09-09 13:38:21 +0200

    Update main.py

--- day_45/main.py
+++ day_45/main.py
@@ -1,11 +1,24 @@
+# from os import name
 from bs4 import BeautifulSoup
+import bs4
+import requests
+from requests.api import head
 
+response = requests.get("https://news.ycombinator.com/")
 
+yc_webpage = response.text
+# print(response.text)
 
+soup = bs4.BeautifulSoup(yc_webpage, "html.parser")
 
-with open(r"day_45\website.html") as file:
-    contents = file.read()
-    
+all_anchor_tags = soup.find_all("a")
 
-soup = BeautifulSoup(contents)
-print(soup)    
+article_tag = soup.find(name = "a", class_="storylink")
+# print(headlines.text)
+article_text = article_tag.text
+article_link = article_tag.get("href")
+article_upvote = soup.find(name = "span", class_="score")
+
+print(article_text)
+print(article_link)
+print(article_upvote)
