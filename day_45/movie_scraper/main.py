82e80024a6bfaa5dc3e54e4ddeeb7d9679254e2b
Siddhesh <64964929+sid-py@users.noreply.github.com>
2021-09-13 23:10:13 +0200

    Update main.py

--- day_45/movie_scraper/main.py
+++ day_45/movie_scraper/main.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
+response = requests.get("https://stackoverflow.com/questions/56478652/scraping-text-in-h3-and-p-tags-using-beautifulsoup-python")
 
 mov_webpage = response.text
 # print(response.text)
@@ -10,6 +10,6 @@ soup = BeautifulSoup(mov_webpage, "html.parser")
 
 # print(soup.prettify)
 
-all_movies = soup.find_all(name="h3", class_="jsx-4245974604")
-print(all_movies)
+all_movies = soup.find(name="h3", class_="flex--item fs-title")
+print(all_movies.text)
 
