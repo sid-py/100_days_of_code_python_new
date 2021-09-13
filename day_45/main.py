aecc8dbd9f100c2b8c61dbe5f436bf36678c871a
Siddhesh <64964929+sid-py@users.noreply.github.com>
2021-09-13 21:42:39 +0200

    Update main.py

--- day_45/main.py
+++ day_45/main.py
@@ -13,12 +13,16 @@ soup = bs4.BeautifulSoup(yc_webpage, "html.parser")
 
 all_anchor_tags = soup.find_all("a")
 
-article_tag = soup.find(name = "a", class_="storylink")
-# print(headlines.text)
-article_text = article_tag.text
-article_link = article_tag.get("href")
-article_upvote = soup.find(name = "span", class_="score")
+article_tags = soup.find_all(name="a", class_= "storylink")
+article_upvote = soup.find_all(name = "span", class_="score")
+for article_headline in article_tags:
+    article_text = article_headline.text
+    article_link = article_headline.get("href")
+    print(article_headline.text)
+    print(article_link)
+for vote_line in article_upvote:
+    votes = vote_line.text
+    vote = votes.split()
+    vote_count = int(vote[0])
+    print(vote_count)
 
-print(article_text)
-print(article_link)
-print(article_upvote)
