3b0386b02bf33a98c59bcfa0bb961bcd4fa94caa
Siddhesh <64964929+sid-py@users.noreply.github.com>
2021-11-01 23:38:25 +0100

    WIP

--- Professional_Portfolio_projects/day_82/bullet.py
+++ Professional_Portfolio_projects/day_82/bullet.py
@@ -2,14 +2,17 @@ from turtle import Turtle
 
 class Bullet(Turtle):
     
-    def __init__(self, position):
+    def __init__(self):
         super().__init__()
         self.shape("square")
         self.shapesize(stretch_wid = 0.5, stretch_len = 0.1)
         self.color("red")
         self.penup()
-        self.goto(position)
+        
         
     def fire_bullet(self):
         new_y = self.ycor() + 10
-        self.goto(self.xcor(), new_y)
+        new_x = self.xcor() + 10
+        self.goto(new_x, new_y)
+        
+        
--- Professional_Portfolio_projects/day_82/main.py
+++ Professional_Portfolio_projects/day_82/main.py
@@ -17,11 +17,12 @@
 from turtle import Turtle, Screen
 from ship import Ship
 from bullet import Bullet
+import time
 
 # -----------------UI INterface-------------------#
 # SPACE_SHIP_LOC = r"C:\Users\2kjph5\OneDrive - Merit Automotive Electronics Systems, S.L\03-Miscellaneous\Studies\Learning\python\100_days_of_code_python_new\Professional_Portfolio_projects\day_82\spaceship2.gif"
 
-SPACE_SHIP_STARTING_POSITION = (0,+230)
+SPACE_SHIP_STARTING_POSITION = (0,-230)
 
 
 screen = Screen()
@@ -29,14 +30,23 @@ screen.bgcolor("black")
 screen.title("Shoot 'em Up!")
 screen.setup(width=500, height=500)
 screen.tracer(5)
+bullet = Bullet()
+screen.tracer(5)
 space_ship = Ship(SPACE_SHIP_STARTING_POSITION)
 
 screen.listen()
 screen.onkeypress(space_ship.go_left, "Left")
 screen.onkeypress(space_ship.go_right, "Right")
 
-bullet = Bullet(SPACE_SHIP_STARTING_POSITION)
+game_is_on = True
 
+while game_is_on:
+    
+    time.sleep(bullet.fire_bullet)
+    screen.update()
+    
+    
+    
 
 
 
